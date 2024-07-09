a593d922e3d90f859f33bb7e69d79deb35edac4a
angeloahm <angeloahm@gmail.com>
2024-07-09 12:53:30 -0500

    Fixing ingest to get CR. Made a .csv with the main variables.

--- ingest_raw.py
+++ ingest_raw.py
@@ -2,8 +2,12 @@ import pandas as pd
 import os
 
 # Define the file paths
-mdrm_path = 'C:/Users/angel/Documents/Economics/Research/Banking Project/data/raw/MDRM'
-cr_path = 'C:/Users/angel/Documents/Economics/Research/Banking Project/data/raw/Call Reports'
+#mdrm_path = 'C:/Users/angel/Documents/Economics/Research/Banking Project/data/raw/MDRM'
+#cr_path = 'C:/Users/angel/Documents/Economics/Research/Banking Project/data/raw/Call Reports'
+
+# Path with FED computer 
+cr_path = 'C:/Users/IRAXA11/Documents/Research/Banking Project/Call Reports'
+mdrm_path = 'C:/Users/IRAXA11/Documents/Research/Banking Project/MDRM'
 
 # Read MDRM_CSV.csv with a tab separator:
 mdrm = pd.read_csv(mdrm_path + '/MDRM_CSV.csv', header=1, sep=',', low_memory=False)
@@ -25,9 +29,10 @@ def ingest(f_paths):
         long['value'] = pd.to_numeric(long['value'], errors='coerce')
         long['year'] = f_path[-16:-12]
         long = pd.merge(long, mdrm_names, left_on='variable', right_on='var_name', how='left')
+        
         # Save 'long' data to a csv file in the folder data/intermediate/call_reports:
-        long.to_csv('C:/Users/angel/Documents/Economics/Research/Banking Project/data/intermediate/call_reports/' + f_path[-16:-12] + '.csv', index=False)
+        #long.to_csv('C:/Users/angel/Documents/Economics/Research/Banking Project/data/intermediate/call_reports/' + f_path[-16:-4] + '.csv', index=False)
+        long.to_csv('C:/Users/IRAXA11/Documents/Research/Banking Project/Call Reports/' + f_path[-16:-4] + '.csv', index=False)
 
+    
 ingest(f_paths)
-
-# Testing remote access. 
