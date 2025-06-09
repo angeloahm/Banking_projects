ff606f6c3810041251ae5a1bf90af71dab9ae4fb
angeloahm <angeloahm@gmail.com>
2025-06-09 16:24:19 -0500

    Separate the mappings and the class in different files. Managed to find the 'charter type' variable. Filter by commercial banks.

---
+++ codes/mappings.py
@@ -0,0 +1,241 @@
+"""
+This module contains the mappings dictionary used for constructing variables in the Call Reports dataset.
+The mappings define how to create various financial variables by combining or transforming existing columns.
+"""
+
+mappings = [
+    # Loans: Total Loans = RCON2122.combine_first(RCFD2122), then mask zeros.
+    {
+        "first_col": "RCON2122",
+        "second_col": "RCON2122",
+        "new_var": "Total Loans",
+        "method": "secondary",   # Gives RCON2122 if available, else RCFD2122.
+        "mask_zeros": True
+    },
+    # Assets: Create Total Assets from RCON2170 and mask zeros.
+    {
+        "first_col": "RCFD2170",
+        "second_col": "RCON2170",
+        "new_var": "Total Assets",
+        "method": "secondary",   # Since both are the same, this simply copies RCON2170.
+        "mask_zeros": True
+    },
+    # Equity Capital: Create Total Equity Capital from RCON3210 and RCFD3210 and mask zeros.
+    {
+        "first_col": "RCON3210",
+        "second_col": "RCFD3210",
+        "new_var": "Total Equity Capital",
+        "method": "secondary",   # Since both are the same, this simply copies RCON3210.
+    },
+    # AOCI: Create AOCI from RCONB530 and RCFDB530 and mask zeros.
+    {
+        "first_col": "RCONB530",
+        "second_col": "RCFDB530",
+        "new_var": "AOCI",
+        "method": "secondary",   # Since both are the same, this simply copies RCONB530.
+    },
+    # ********************************************************************************************************
+    # ****************************************** Security Variables ******************************************
+    # ********************************************************************************************************
+    # ------------------------------------------ INTERMEDIATE STEPS ------------------------------------------
+    # Create RCON1754_right = RCON1754_x.combine_first(RCON1754)
+    {
+        "new_var":              "RCON1754_right",
+        "first_col":            "RCON1754",      # fallback column
+        "second_col":           "RCON1754_x",   # primary column
+        "method":               "secondary"
+    },
+    # Create RCFD1754_right = RCFD1754_x.combine_first(RCFD1754)
+    {
+        "new_var":              "RCFD1754_right",
+        "first_col":            "RCFD1754",      # fallback column
+        "second_col":           "RCFD1754_x",   # primary column
+        "method":               "secondary"
+    },
+    # Create 1754_right from RCFD1754_right and RCON1754_right
+    # This takes the row-wise minimum:
+    {
+        "new_var":              "1754_right",
+        "first_col":            "RCFD1754_right",
+        "second_col":           "RCON1754_right",
+        "method":               "secondary"
+    },
+    # Create '1771_right' from RCON1771 and RCFD1771:
+    {
+        "new_var":              "1771_right",
+        "first_col":            "RCFD1771",
+        "second_col":           "RCON1771",
+        "method":               "secondary"
+    },
+    # Create 1772_right from RCFD1772 and RCON1772:
+    {
+        "new_var":              "1772_right",
+        "first_col":            "RCFD1772",
+        "second_col":           "RCON1772",
+        "method":               "secondary"
+    },
+    # Create B989_right from RCFDB989 and RCONB989:
+    {
+        "new_var":              "B989_right",
+        "first_col":            "RCFDB989",
+        "second_col":           "RCONB989",
+        "method":               "secondary",
+    },
+    # ------------------------------------------ VARIABLES ------------------------------------------
+    {
+        "new_var":          "HTM Securities",
+        "first_col":        "1754_right",       
+        "second_col":       "1754_right",
+        "switch_date":      "2019-03-31",           # First date with the new MDRM code.
+        "first_col_post":   "RCFDJJ34",
+        "second_col_post":  "RCONJJ34",
+        "method":           "secondary",
+    },
+    # Create AFS Securities from RCON1773 and RCFD1773_x:
+    {
+        "new_var":          "AFS Securities",
+        "first_col":        "RCFD1773_x",
+        "second_col":       "RCON1773",
+        "method":           "secondary",
+    },
+    # Create 'Securities AC' from 1754_right and 1772_right:
+    {
+        "new_var":          "Securities AC",
+        "first_col":        "1754_right",
+        "second_col":       "1772_right",
+        "method":           "sum",
+    },
+    # Create 'Securities Fair Value' from 1771_right and AFS Securities:
+    {
+        "new_var":          "Securities FV",
+        "first_col":        "1771_right",
+        "second_col":       "AFS Securities",
+        "method":           "sum",
+    },
+    # Create 'Equity Securities' from 'RCFDJA22' and 'RCONJA22':
+    {
+        "new_var":          "Equity Securities",
+        "first_col":        "RCFDJA22",
+        "second_col":       "RCONJA22",
+        "method":           "secondary",
+    },
+    # Create FFS from B989_right and RCONB987:
+    {
+        "new_var":          "FFS",
+        "first_col":        "B989_right",
+        "second_col":       "RCONB987",
+        "method":           "sum",
+    },
+    # ********************************************************************************************************
+    # ************************************************* Cash *************************************************
+    # ********************************************************************************************************
+    # Create 0081_right from RCFD0081 and RCON0081:
+    {
+        "new_var":          "0081_right",
+        "first_col":        "RCFD0081",
+        "second_col":       "RCON0081",
+        "method":           "secondary",   
+    },
+    # Create 0071_right from RCFD0071 and RCON0071:
+    {
+        "new_var":          "0071_right",
+        "first_col":        "RCFD0071",
+        "second_col":       "RCON0071",
+        "method":           "secondary",   
+    },
+    # Create 'Cash' from 0081_right and 0071_right:
+    {
+        "new_var":          "Cash",
+        "first_col":        "0081_right",
+        "second_col":       "0071_right",
+        "method":           "sum",
+        "mask_zeros":       True
+    },
+    # ********************************************************************************************************
+    # *********************************************** Deposits ***********************************************
+    # ********************************************************************************************************
+    # Create Total Deposits from RCON2200 (a simple copy) and mask zeros.
+    {
+        "new_var":          "Total Deposits",
+        "first_col":        "RCON2200",
+        "second_col":       "RCON2200",
+        "method":           "secondary",   # Since both are the same, this simply copies RCON2200.
+        "mask_zeros":       True
+    },
+    # Create 'Insured Deposit Accounts' from RCONF049 and RCONF045:
+    {
+        "new_var":          "Insured Deposit Accounts",
+        "first_col":        "RCONF049",
+        "second_col":       "RCONF045",
+        "method":           "sum",
+        "mask_zeros":       True
+    },
+    # Create 'Number of Insured Deposit Accounts' from RCONF050 and RCONF046:
+    {
+        "new_var":          "Number of Insured Deposit Accounts",
+        "first_col":        "RCONF050",
+        "second_col":       "RCONF046",
+        "method":           "sum",
+    },
+    # Create 'Uninsured Deposit Accounts' from RCONF051 and RCONF047:
+    {
+        "new_var":          "Uninsured Deposit Accounts",
+        "first_col":        "RCONF051",
+        "second_col":       "RCONF047",
+        "method":           "sum",
+    },
+    # Create 'Number of Uninsured Deposit Accounts' from RCONF052 and RCONF048:
+    {
+        "new_var":          "Number of Uninsured Deposit Accounts",
+        "first_col":        "RCONF052",
+        "second_col":       "RCONF048",
+        "method":           "sum",
+    },
+    # Create 'Transaction Deposits' from RCON2215 (a simple copy) and mask zeros.
+    {
+        "new_var":          "Transaction Deposits",
+        "first_col":        "RCON2215",
+        "second_col":       "RCON2215",
+        "method":           "secondary",   # Since both are the same, this simply copies RCON2215.
+        "mask_zeros":       True
+    },
+    # total time deposits (small + large)
+    {
+        "new_var":          "Time Deposits",
+        "first_col":        "RCON6648",       # < $100 k
+        "second_col":       "RCON2604",       # ≥ $100 k (pre-2010)
+        "method":           "sum",
+        "mask_zeros":       True
+    },
+    # ********************************************************************************************************
+    # ****************************************** Interest Expenses *******************************************
+    # ********************************************************************************************************
+    # Create 'Transaction Deposit Expenses' from RIAD4508:
+    {
+        "new_var":          "Transaction Deposit Expenses",
+        "first_col":        "RIAD4508",
+        "second_col":       "RIAD4508",  # Since both are the same, this simply copies RCON2215.
+        "method":           "secondary",
+        "mask_zeros":       True
+    },
+    # Create 'Savings Expenses' fro RIAD0093:
+    {
+        "new_var":          "Savings Expenses",
+        "first_col":        "RIAD0093",
+        "second_col":       "RIAD0093",  # Since both are the same, this simply copies RCON2215.
+        "method":           "secondary",
+        "mask_zeros":       True
+    },
+    # Create 'Time Deposit Expenses' from RIADA517 and RIADA518 (pre-2017) or RIADHK03 and RIADHK04 (post-2017):
+    {
+        "new_var":          "Time Deposit Expenses",
+        # pre-2017 codes (FDIC cap = $100k)
+        "first_col":        "RIADA518",      # interest on TDs < $100k
+        "second_col":       "RIADA517",      # interest on TDs ≥ $100k
+        # post-2017 codes (FDIC cap = $250k)
+        "switch_date":      "2017-03-31",    # first quarter with HK-codes
+        "first_col_post":   "RIADHK03",      # interest on TDs ≤ $250k
+        "second_col_post":  "RIADHK04",      # interest on TDs  > $250k
+        "method":           "sum"            # add the two buckets each quarter
+    }
+] 
